a484eda32b348ae2756923b8105731d899e7d29e
Dave Abrahams <dave@boostpro.com>
2010-04-11 12:19:04 -0400

    Fixed bugs in info gathering

--- tests/test_correct_pip.py
+++ tests/test_correct_pip.py
@@ -1,6 +1,6 @@
 
 from os.path import abspath, join, dirname, pardir
-from test_pip import here, reset_env, run_pip, pyversion, lib_py
+from test_pip import here, reset_env, get_env, run_pip, pyversion, lib_py
 
 def test_correct_pip_version():
     """
@@ -20,9 +20,12 @@ def test_correct_pip_version():
     dir = re.match(r'\s*pip\s\S+\sfrom\s+(.*)\s\([^(]+\)$', result.stdout.replace('\r\n','\n')).group(1)
 
     import sys
-    args = (sys.executable, '-c', 'import pip; print "pip module at:", pip.__file__', '-E', env.base_path) + args
-    env.run(*args, **kw)
+    env = get_env()
 
+    print env.run( 
+        sys.executable, '-c', 'import pip; print "loaded pip module at:", pip.__file__', '-E', env.base_path 
+        ).stdout
+    
     diffs = filecmp.dircmp(join(base,'pip'), join(dir,'pip'))
 
     # If any non-matching .py files exist, we have a problem: run_pip
