358b0bca267dcf65b1a53764197a5dff62992519
Francesco <f.guerrieri@gmail.com>
2010-04-11 21:49:06 +0200

    dir -> pip_dir to avoid shadowing a built-in

--- tests/test_correct_pip.py
+++ tests/test_correct_pip.py
@@ -1,4 +1,4 @@
-
+import test_pip
 from os.path import abspath, join, dirname, pardir
 from test_pip import here, reset_env, get_env, run_pip, pyversion, lib_py
 
@@ -17,7 +17,7 @@ def test_correct_pip_version():
 
     # compare the directory tree of the invoked pip with that of this source distribution
     import re,filecmp
-    dir = re.match(r'\s*pip\s\S+\sfrom\s+(.*)\s\([^(]+\)$', result.stdout.replace('\r\n','\n')).group(1)
+    pip_dir = re.match(r'\s*pip\s\S+\sfrom\s+(.*)\s\([^(]+\)$', result.stdout.replace('\r\n','\n')).group(1)
 
     import sys
     env = get_env()
@@ -26,11 +26,11 @@ def test_correct_pip_version():
         sys.executable, '-c', 'import pip; print "loaded pip module at:", pip.__file__', '-E', env.base_path 
         ).stdout
     
-    diffs = filecmp.dircmp(join(base,'pip'), join(dir,'pip'))
+    diffs = filecmp.dircmp(join(base,'pip'), join(pip_dir,'pip'))
 
     # If any non-matching .py files exist, we have a problem: run_pip
     # is picking up some other version!  N.B. if this project acquires
     # primary resources other than .py files, this code will need
     # maintenance
     mismatch_py = [x for x in diffs.left_only + diffs.right_only + diffs.diff_files if x.endswith('.py')]
-    assert not mismatch_py, 'mismatched source files in %r and %r'% (join(base,'pip'), join(dir,'pip'))
+    assert not mismatch_py, 'mismatched source files in %r and %r'% (join(base,'pip'), join(pip_dir,'pip'))
