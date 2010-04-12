52e03e0bc162c92e2c4389ca831fb44b9ca83fba
Francesco <f.guerrieri@gmail.com>
2010-04-13 01:20:02 +0200

    bundle creation test pass

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -2,8 +2,8 @@
 import zipfile
 import textwrap
 from os.path import abspath, join, dirname, pardir
-from test_pip import here, reset_env, run_pip, pyversion, lib_py
-from test_pip import write_file
+from test_pip import here, reset_env, run_pip, pyversion, write_file #, lib_py
+from path import Path
 
 def test_create_bundle():
     """
@@ -12,17 +12,21 @@ def test_create_bundle():
     index (pip itself).
 
     """
-    reset_env()
-    fspkg = 'file://%s/FSPkg' %join(here, 'packages')
+    env = reset_env()
+    fspkg = 'file://%s/FSPkg' % join(here, 'packages')
     dummy = run_pip('install', '-e', fspkg)
     pkg_lines = textwrap.dedent('''\
             -e %s
             -e svn+http://svn.colorstudy.com/INITools/trunk#egg=initools-dev
             pip''' % fspkg)
     write_file('bundle-req.txt', pkg_lines)
-    result = run_pip('bundle', '-r', 'bundle-req.txt', 'test.pybundle')
+    # Create a bundle in env.base_path/ test.pybundle
+    result = run_pip('bundle', '-r', env.base_path/ 'bundle-req.txt', env.base_path/ 'test.pybundle')
     bundle = result.files_after.get('test.pybundle', None)
+    from pprint import pprint
+    pprint(result.files_after)
     assert bundle is not None
+
     files = zipfile.ZipFile(bundle.full).namelist()
     assert 'src/FSPkg/' in files
     assert 'src/initools/' in files
